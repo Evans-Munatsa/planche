fddc5fcc2538521989a8efbd34d8614d175ec626
Evans-Munatsa <gagamel.em@gmail.com>
2016-12-08 10:59:46 +0200

    created a feature spec for signup

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "sign_up" do
+
+  before do
+    visit root_path
+  end
+
+  scenario "visiting the site to sign up" do
+    click_link "Sign up"
+    expect(page.current_path).to eq('/users/sign_up')
+    fill_in("Email", with: "tester1@test.com")
+    fill_in("Password", with: "password123")
+    fill_in("Password confirmation", with: "password123")
+    click_button('Sign up')
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+end
