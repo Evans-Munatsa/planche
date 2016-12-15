eb76a02ac595957c3574b7f40a8199594626a573
Evans-Munatsa <gagamel.em@gmail.com>
2016-12-15 22:24:21 +0200

    sorted gem pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213105540) do
+ActiveRecord::Schema.define(version: 20161215201453) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
