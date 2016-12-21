171bce4009bee193ea082041f534854048d144fb
Evans-Munatsa <gagamel.em@gmail.com>
2016-12-21 15:37:13 +0200

    analytics

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
-  resources :posts
-  resources :categories
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
+
+  resources :categories
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
