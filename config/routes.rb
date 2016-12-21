695f2541d8119eda801250e8baf4e5626e4c5047
Evans-Munatsa <gagamel.em@gmail.com>
2016-12-21 15:39:35 +0200

    nested routes for categories and posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
 
-  resources :categories
-  resources :posts
+  resources :categories do
+  	resources :posts
+  end
+  # resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
