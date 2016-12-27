0e7fa38e44d1d534e94cafedc4ecaf0818448537
Evans-Munatsa <gagamel.em@gmail.com>
2016-12-27 18:51:19 +0200

    references

--- db/migrate/20161208102950_create_posts.rb
+++ db/migrate/20161208102950_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :categories, foreign_key: true
+      t.integer :tcategory_id, index: true, foreign_key: true
 
       t.timestamps
     end
