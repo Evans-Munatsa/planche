24fe631cf024931379734cbb3ed22d87ceda674b
Evans-Munatsa <gagamel.em@gmail.com>
2017-02-23 19:38:42 +0200

    modifications

--- db/migrate/20161221134026_create_comments.rb
+++ db/migrate/20161221134026_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
       t.text :comment
 
       t.integer :post_id, index: true, foreign_key: true
+      t.integer :user_id, index: true, foreign_key: true
       t.timestamps
     end
   end
