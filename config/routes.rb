becbb385e245668dad087f050b10040eecaeea39
Evans-Munatsa <gagamel.em@gmail.com>
2017-03-16 12:42:45 +0200

    configuring links

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, :controllers => { registrations: 'registrations' }
+  
   root 'static_pages#home'
   get 'static_pages/about'
 
   resources :categories
-
   resources :posts do
     member do
       put "like", to: "posts#upvote"
